fd0d9b30eb7bf452868f7fd21a9ff73299f2bad6
Billy Barbaro <billy@thebarbaros.com>
2015-11-04 01:32:47 -0500

    Tests for limitedInfection

--- src/UserGraphTest.java
+++ src/UserGraphTest.java
@@ -34,7 +34,7 @@ public class UserGraphTest {
     public void testTotalInfection() {
         UserGraph testGraph = generateTestUserGraph();
         Version newVersion = new Version(1, 1, 1);
-        testGraph.totalInfection(testGraph.getUser("Student1"), newVersion);
+        assertEquals("Incorrect number of infections", testGraph.totalInfection(testGraph.getUser("Student1"), newVersion), 7);
         for (String username : testGraph.getUsers().keySet()) {
             User user = testGraph.getUser(username);
             if (!username.equals("Student6")) {
@@ -44,5 +44,108 @@ public class UserGraphTest {
                 assertEquals("User infected but shouldn't be", new Version(1, 0, 0), user.getCurrentVersion());
             }
         }
+    }
+
+    @Test
+     public void testLimitedInfection0() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 0, testGraph.limitedInfection(testGraph.getUser("Student1"), newVersion, 0));
+        for (String username : testGraph.getUsers().keySet()) {
+            User user = testGraph.getUser(username);
+            assertEquals("User infected but shouldn't be", new Version(1, 0, 0), user.getCurrentVersion());
+        }
+    }
+
+    @Test
+    public void testLimitedInfection1() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 1, testGraph.limitedInfection(testGraph.getUser("Student1"), newVersion, 1));
+        for (String username : testGraph.getUsers().keySet()) {
+            User user = testGraph.getUser(username);
+            if (username.equals("Student1")) {
+                assertEquals("User not infected", new Version(1, 1, 1), user.getCurrentVersion());
+            }
+            else {
+                assertEquals("User infected but shouldn't be", new Version(1, 0, 0), user.getCurrentVersion());
+            }
+        }
+    }
+
+    @Test
+    public void testLimitedInfectionIsolated() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 1, testGraph.limitedInfection(testGraph.getUser("Student6"), newVersion, 1000));
+        for (String username : testGraph.getUsers().keySet()) {
+            User user = testGraph.getUser(username);
+            if (username.equals("Student6")) {
+                assertEquals("User not infected", new Version(1, 1, 1), user.getCurrentVersion());
+            }
+            else {
+                assertEquals("User infected but shouldn't be", new Version(1, 0, 0), user.getCurrentVersion());
+            }
+        }
+    }
+
+    @Test
+    public void testLimitedInfectionTwoWaves() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 4, testGraph.limitedInfection(testGraph.getUser("Student1"), newVersion, 4));
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student1").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student2").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student3").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student4").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student5").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student6").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Coach1").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Coach2").getCurrentVersion());
+    }
+
+    @Test
+    public void testLimitedInfectionThresholdExceeded() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 4, testGraph.limitedInfection(testGraph.getUser("Student1"), newVersion, 3));
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student1").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student2").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student3").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student4").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student5").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student6").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Coach1").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Coach2").getCurrentVersion());
+    }
+
+    @Test
+    public void testLimitedInfectionMultipleCoaches() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 7, testGraph.limitedInfection(testGraph.getUser("Student3"), newVersion, 4));
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student1").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student2").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student3").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student4").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student5").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student6").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Coach1").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Coach2").getCurrentVersion());
+    }
+
+    @Test
+    public void testLimitedInfectionFromCoach() {
+        UserGraph testGraph = generateTestUserGraph();
+        Version newVersion = new Version(1, 1, 1);
+        assertEquals("Incorrect number of infections", 4, testGraph.limitedInfection(testGraph.getUser("Coach1"), newVersion, 2));
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student1").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student2").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Student3").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student4").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student5").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Student6").getCurrentVersion());
+        assertEquals("User not infected", new Version(1, 1, 1), testGraph.getUser("Coach1").getCurrentVersion());
+        assertEquals("User infected but shouldn't be", new Version(1, 0, 0), testGraph.getUser("Coach2").getCurrentVersion());
     }
 }
