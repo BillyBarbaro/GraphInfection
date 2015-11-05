628fb86f18cecbacbd8a4e1e2edf24c669375e61
Billy Barbaro <billy@thebarbaros.com>
2015-11-05 03:19:26 -0500

    Swapped out the random function and started checking users not on the correct version.

--- src/BulkTest.java
+++ src/BulkTest.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class BulkTest {
 
@@ -15,20 +15,25 @@ public class BulkTest {
     private static final int MAX_NUMBER_COACHES_FOR_STUDENT = 8;
 
 
+    private static int getRandom(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
     private static void addTeamToGraph(UserGraph graph, int teamNumber) {
-        int numberOfCoaches = ThreadLocalRandom.current().nextInt(MIN_NUMBER_COACHES, MAX_NUMBER_COACHES + 1);
+        int numberOfCoaches = getRandom(MIN_NUMBER_COACHES, MAX_NUMBER_COACHES);
         User coaches[] = new User[numberOfCoaches];
         for (int i = 0; i < numberOfCoaches; i++) {
             coaches[i] = new User.Builder().build("Coach" + teamNumber + "_" + i);
             graph.addUser(coaches[i]);
         }
 
-        int numberOfStudents = ThreadLocalRandom.current().nextInt(MIN_NUMBER_STUDENTS, MAX_NUMBER_STUDENTS + 1);
+        int numberOfStudents = getRandom(MIN_NUMBER_STUDENTS, MAX_NUMBER_STUDENTS);
         for (int i = 0; i < numberOfStudents; i++) {
             User student = new User.Builder().build("Student" + teamNumber + "_" + i);
-            int studentCoachCount =  ThreadLocalRandom.current().nextInt(MIN_NUMBER_COACHES_FOR_STUDENT, MAX_NUMBER_COACHES_FOR_STUDENT + 1);
+            int studentCoachCount =  getRandom(MIN_NUMBER_COACHES_FOR_STUDENT, MAX_NUMBER_COACHES_FOR_STUDENT);
             for (int j = 0; j < studentCoachCount; j++) {
-                student.addCoach(coaches[ThreadLocalRandom.current().nextInt(0, numberOfCoaches)]);
+                student.addCoach(coaches[getRandom(0, numberOfCoaches - 1)]);
             }
             graph.addUser(student);
         }
@@ -46,12 +51,24 @@ public class BulkTest {
 
         System.out.println("Desired\tActual\tRatio");
         for (Team team : teams) {
-            int desiredInfected = ThreadLocalRandom.current().nextInt(1, team.getSize() + 1);
-            int actualInfected = testGraph.limitedInfection(team.getTeamMembers().get(0), new Version(1, 1, 1), desiredInfected);
-            ratios.add((double)actualInfected/desiredInfected);
-            System.out.println(desiredInfected + "\t" + actualInfected + "\t" + (double)actualInfected/desiredInfected);
+            Version newVersion = new Version(1, 1, 1);
+            int desiredInfected = getRandom(1, team.getSize());
+            int actualInfected = testGraph.limitedInfection(team.getTeamMembers().get(0), newVersion, desiredInfected);
+            ratios.add((double) actualInfected / desiredInfected);
+            //System.out.println(desiredInfected + "\t" + actualInfected + "\t" + (double)actualInfected/desiredInfected);
 
-            // Check student to teacher same version ratio.
+            for (User user : team.getTeamMembers()) {
+                if (user.getUserName().contains("Coach")) {
+                    int upgraded = 0;
+                    for (User student : user.getStudents()) {
+                        if (student.getCurrentVersion().equals(newVersion)) {
+                            upgraded++;
+                        }
+                    }
+                    System.out.println(user.getStudents().size()-upgraded + "\t" + upgraded + "\t" + (double)upgraded/user.getStudents().size());
+
+                }
+            }
         }
         System.out.println("Average ratio of actual to desired: " + (ratios.stream().mapToDouble(a -> a).average()).getAsDouble());
     }
