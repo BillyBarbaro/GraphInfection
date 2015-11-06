d5f52f3387c506e811dcc0b0ff3502fb396e88b2
Billy Barbaro <billy@thebarbaros.com>
2015-11-06 15:28:50 -0500

    Refactors to the User class.

--- src/User.java
+++ src/User.java
@@ -1,9 +1,13 @@
 import java.util.LinkedList;
 
+/*
+    A class representing a user of the application
+ */
 public class User {
 
     private static final Version LATEST_VERSION = new Version(1, 0, 0);
 
+    // Builder pattern for adding relations when creating a user.
     public static class Builder {
         private LinkedList<User> coaches = new LinkedList<>();
         private LinkedList<User> students = new LinkedList<>();
@@ -45,30 +49,28 @@ public class User {
     private LinkedList<User> coaches;
     private LinkedList<User> students;
 
+    // Called only using the builder
     private User(String userName, Builder userBuilder) {
         this.userName = userName;
         currentVersion = LATEST_VERSION;
         coaches = userBuilder.getCoaches();
+
+        // For every relation, add the reciprocal to the other user.
         for (User coach : coaches) {
             coach.addStudentNotReciprocal(this);
         }
+
         students = userBuilder.getStudents();
         for (User student : students) {
             student.addCoachNotReciprocal(this);
         }
     }
 
-    User(User user) {
-        this.userName = user.getUserName();
-        this.currentVersion =user.getCurrentVersion();
-        this.coaches = user.getCoaches();
-        this.students = user.getStudents();
-    }
-
     public String getUserName() {
         return userName;
     }
 
+    // Return a new instance of version so it can't be modified outside the setter.
     public Version getCurrentVersion() {
         return new Version(currentVersion);
     }
@@ -77,53 +79,59 @@ public class User {
         this.currentVersion = currentVersion;
     }
 
+    // Returns copy of the list of students.  Mutating the list can't mutate the list for the class
     public LinkedList<User> getStudents() {
         return new LinkedList<>(students);
     }
 
-    private void addStudentNotReciprocal(User newStudent) {
-        students.add(newStudent);
-    }
-
+    // Add a student to the user.  Also adds this user as a coach to the student passed in.
     public User addStudent(User newStudent) {
         addStudentNotReciprocal(newStudent);
         newStudent.addCoachNotReciprocal(this);
         return this;
     }
 
-    public User removeStudentNotReciprocal(User student) {
-        students.remove(student);
-        return this;
+    private void addStudentNotReciprocal(User newStudent) {
+        students.add(newStudent);
     }
 
+    // Removes a student from the User.  Also remove this user from the student's list of coaches.
     public User removeStudent(User student) {
         removeStudentNotReciprocal(student);
         student.removeCoachNotReciprocal(this);
         return this;
     }
 
+    private User removeStudentNotReciprocal(User student) {
+        students.remove(student);
+        return this;
+    }
+
+    // Returns copy of the list of coaches.  Mutating the list can't mutate the list for the class
     public LinkedList<User> getCoaches() {
         return new LinkedList<>(coaches);
     }
 
-    private void addCoachNotReciprocal(User newCoach) {
-        coaches.add(newCoach);
-    }
-
+    // Add a student to the user.  Also adds this user as a student to the coach passed in.
     public User addCoach(User newCoach) {
         addCoachNotReciprocal(newCoach);
         newCoach.addStudentNotReciprocal(this);
         return this;
     }
 
-    private void removeCoachNotReciprocal(User coach) {
-        coaches.remove(coach);
+    private void addCoachNotReciprocal(User newCoach) {
+        coaches.add(newCoach);
     }
 
+    // Removes a coach from the User.  Also remove this user from the coach's list of students.
     public User removeCoach(User coach) {
         removeCoachNotReciprocal(coach);
         coach.removeStudentNotReciprocal(this);
         return this;
+    }
+
+    private void removeCoachNotReciprocal(User coach) {
+        coaches.remove(coach);
     }
 
     public String toString() {
